ee422cf7979372b89b5341476ae71cc4be04e6c4
ice <icecream.6639@gmail.com>
2023-06-27 20:53:26 +0800

    init

---
+++ src/main/java/com/juc/common/JucUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.juc.juc_utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JucUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JucUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juc/juc_utils/JucUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juc.juc_utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
